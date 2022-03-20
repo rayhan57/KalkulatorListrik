e04e8319546f8f1689c0a7a1f8105ef04fd7436d
Rayhan Lingga B <61607923+rayhan57@users.noreply.github.com>
2022-03-20 12:38:59 +0700

    Add scroll active

--- script.js
+++ script.js
@@ -61,7 +61,21 @@ function Hitung() {
   });
 }
 
+// Navbar Klik
 $(".nav-link").on("click", function () {
   $(".nav-link").removeClass("active");
   $(this).addClass("active");
 });
+
+// Navbar Scroll
+function activeMenu() {
+  const link = document.querySelectorAll(".nav-link");
+  const section = document.querySelectorAll("section");
+  let length = section.length;
+
+  while (--length && window.scrollY + 100 < section[length].offsetTop) {}
+  link.forEach((ltx) => ltx.classList.remove("active"));
+  link[length].classList.add("active");
+}
+activeMenu();
+window.addEventListener("scroll", activeMenu);
